6b44c36f5f0b34d16b352f56fe09a10f4544bd6d
2025050214 <jeejee637@kau.kr>
2026-05-27 15:49:12 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -8,14 +8,17 @@ int main() {
     
     scanf("%d", &N);
     
+    // 첫번째 배열 입력
     for (p = a; p < a + N; p++) {
         scanf("%d", p);
     }
 
+    // 두번째 배열 입력
     for (q = b; q < b + N; q++) {
            scanf("%d", q);
     }
-     
+
+    // 역방향으로 더해서 출력
     for (p = a, q = b + N - 1; p < a + N; p++, q--) {
         printf(" %d", *p + *q);
     }

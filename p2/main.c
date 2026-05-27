584d2aa410f11fe444500be40b473ecff8f6134d
2025050214 <jeejee637@kau.kr>
2026-05-27 15:51:28 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int N;
-    int a[20], b[20];
+    int N; // 입력받을 정수 개수
+    int a[20], b[20]; // 두 개의 배열 선언
     
-    int *p, *q;
-    
+    int *p, *q; // 포인터 변수 선언
+
+    // N 입력
     scanf("%d", &N);
     
     // 첫번째 배열 입력
@@ -18,7 +19,7 @@ int main() {
            scanf("%d", q);
     }
 
-    // 역방향으로 더해서 출력
+    // 첫번째 배열은 앞에서부터, 두번째 배열은 뒤에서부터 더해서 출력
     for (p = a, q = b + N - 1; p < a + N; p++, q--) {
         printf(" %d", *p + *q);
     }
